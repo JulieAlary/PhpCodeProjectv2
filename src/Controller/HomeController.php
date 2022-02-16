f0df41fb427c3414465de834bd946412557fcb05
JulieAlary <julie.alary@gmail.com>
2022-02-16 17:08:41 +0100

    first route & webpack

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Julie Alary <julie.alary@mail.com>
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+
+        return $this->render('home/index.html.twig');
+    }
+}
